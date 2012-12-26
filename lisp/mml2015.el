fb4b44b34a2be1a1c35d7d9b15b82f04e0a6c215
Adam Sjøgren <asjo@koldfront.dk>
2012-12-26 17:35:55 +0100

    Fix XEmacs 21.5 compilation failure. Really.
    * mml2015.el (mml2015-epg-key-image): protect call to
    set-buffer-multibyte with featurep 'xemacs for XEmacs
    21.5 compability.

--- lisp/mml2015.el
+++ lisp/mml2015.el
@@ -823,8 +823,7 @@ If set, it overrides the setting of `mml2015-sign-with-sender'."
 (defun mml2015-epg-key-image (key-id)
   "Return the image of a key, if any"
   (with-temp-buffer
-    (when (fboundp 'set-buffer-multibyte)
-      (set-buffer-multibyte nil))
+    (unless (featurep 'xemacs) (set-buffer-multibyte nil))
     (let* ((coding-system-for-write 'binary)
            (coding-system-for-read 'binary)
            (data (shell-command-to-string
