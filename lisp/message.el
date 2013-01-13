da189c014ccaf35e58946ad10fef8202d4e63818
Richard Stallman <rms@gnu.org>
2013-01-13 22:55:45 +0000

    message.el (message-forward-make-body-mime): New args BEG, END  specify what part of FORWARD-BUFFER to use.  Do the work directly  instead of calling `mml-insert-buffer'.

--- lisp/message.el
+++ lisp/message.el
@@ -7420,12 +7420,13 @@ Optional DIGEST will use digest to forward."
 	(dolist (elem ignored)
 	  (message-remove-header elem t))))))
 
-(defun message-forward-make-body-mime (forward-buffer)
+(defun message-forward-make-body-mime (forward-buffer &optional beg end)
   (let ((b (point)))
     (insert "\n\n<#part type=message/rfc822 disposition=inline raw=t>\n")
     (save-restriction
       (narrow-to-region (point) (point))
-      (mml-insert-buffer forward-buffer)
+      (insert-buffer-substring forward-buffer beg end)
+      (mml-quote-region (point-min) (point-max))
       (goto-char (point-min))
       (when (looking-at "From ")
 	(replace-match "X-From-Line: "))
